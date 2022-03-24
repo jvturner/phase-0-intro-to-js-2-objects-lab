7c19b5d170066df111bc5a14133ba0ae720794a6
Jamond Turner <jamondturner@yahoo.com>
2022-03-23 22:29:49 -0400

    Progress

--- index.js
+++ index.js
@@ -12,8 +12,15 @@ const employee = {
 }
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
     return { name: 'Sam', streetAddress: '11 Broadway' }
+    
+    
+     {
+       
+    }
 }
+
 
 // Test #2 
 // destructivelyUpdateEmployeeWithKeyAndValue--Destructive
@@ -23,20 +30,40 @@ function updateEmployeeWithKeyAndValue(employee, key, value) {
 // name: 'Sam' streetAddress: '12 Broadway'
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    let name = ['Sam']
+    let streetAddress = ['12 Broadway']
+
+   const obj = [...name, streetAddress]
+
+    console.log(obj)
+
+  
+
+
 }
-const newEmployee = employee
+
+
+
+
+
+
+
+
+
+// const newEmployee = employee
 
 
 
     // Test #3
     // deleteFromEmployeeByKey(employee, key)--Non Destructive
-    // deletes `key` from a clone of employee and returns the 
+    // deletes `key` from a clone of employee and returns the
     // new employee (it is non-destructive):
     // let newEmployee = deleteFromEmployeeByKey
 
-    function deleteFromEmployeeByKey(employee,key) { 
+    // function deleteFromEmployeeByKey(employee,key) {
 
-    }
+    // }
 
 
 // Test #4
@@ -45,7 +72,10 @@ const newEmployee = employee
 // let newEmployee = deleteFromEmployeeByKey
 
 
-function deleteFromEmployeeByKey(employee,key) { }
+// function deleteFromEmployeeByKey(employee,key) { 
+
+
+// }
 
 // Test #5
 // destructivelyDeleteFromEmployeeByKey(employee, key)
@@ -53,19 +83,19 @@ function deleteFromEmployeeByKey(employee,key) { }
 // let newEmployee = destructivelyDeleteFromEmployeeByKey
 
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    return {
-        ...employee.key,
-        delete: key
-    }
-}
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     return {
+//         ...employee.key,
+//         delete: key
+//     }
+// }
 
 // Test #6
 // destructivelyDeleteFromEmployeeByKey(employee, key)
 // modifies the original employee:
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
 
-    return { delete: employee }
-}
+//     return { delete: employee }
+// }
 
